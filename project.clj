c38a80352465a44edb48105a290903401fcb5f28
Diogo Souza <diogo@diogok.net>
2014-03-06 17:44:22 -0300

    json dep updated

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/data.xml "0.0.7"]
                  [org.clojure/data.csv "0.1.2"]
-                 [org.clojure/data.json "0.2.2"]
+                 [org.clojure/data.json "0.2.4"]
                  [dk.ative/docjure "1.6.0"]
                  [clj-http "0.7.6"]]
   :profiles {:dev {:dependencies [[midje "1.5.1"]]
