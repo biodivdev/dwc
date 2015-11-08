985f5a9f6c3894aa214ff9fabc0d93d0907daa53
Diogo Souza da Silva <diogo@diogok.net>
2015-11-07 22:13:53 -0200

    removed unused dep that had trouble with non maven central repo

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject dwc-io "0.0.45"
+(defproject dwc-io "0.0.46"
   :description "Simple (and rather limited) reader/consumer for DwC-A, DIGIR, TAPIR, CSV and xlsx resources, local or online."
   :url "http://github.com/diogok/dwc-io"
   :license {:name "MIT"}
@@ -8,7 +8,6 @@
                  [org.clojure/data.json "0.2.5"]
                  [dk.ative/docjure "1.6.0"]
                  [bigml/closchema "0.5"]
-                 [diogok/cljts "0.4.3"]
                  [clj-http "0.7.6"]]
   :repositories [["clojars" {:sign-releases false}]]
   :profiles {:uberjar {:aot :all}
