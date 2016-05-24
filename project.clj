fdae15c46349c3cdf2c8c2bf43e2a0dbbe7d2c92
Diogo Silva <diogo@diogok.net>
2016-05-24 20:24:04 -0300

    dependencies update

--- project.clj
+++ project.clj
@@ -1,18 +1,18 @@
-(defproject dwc-io "0.0.55"
+(defproject dwc-io "0.0.56"
   :description "Simple (and rather limited) reader/consumer for DwC-A, DIGIR, TAPIR, CSV and xlsx resources, local or online."
   :url "http://github.com/diogok/dwc-io"
   :license {:name "MIT"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/data.xml "0.0.8"]
                  [org.clojure/data.csv "0.1.3"]
-                 [org.clojure/data.json "0.2.5"]
+                 [org.clojure/data.json "0.2.6"]
                  [dk.ative/docjure "1.9.0"]
                  [bigml/closchema "0.6.1"]
-                 [clj-http "2.0.0"]]
+                 [clj-http "2.2.0"]]
   :repositories [["clojars" {:sign-releases false}]]
   :global-vars {*warn-on-reflection* true}
   :profiles {:uberjar {:aot :all}
              :jar {:aot :all}
              :dev {
                    :dependencies [[midje "1.8.2"]]
-                   :plugins [[lein-midje "3.1.3"]]}})
+                   :plugins [[lein-midje "3.2"]]}})
